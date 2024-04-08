f3e8a856583730920699cf0bd2df0d0abe69dfdc
moba0222 <mohammad.babu.miya@netcracker.com>
2024-04-08 14:27:33 +0530

    08-04-2024 - tech learning starting-001

--- Angular/Code/My-Project/practice-project/src/app/app.component.ts
+++ Angular/Code/My-Project/practice-project/src/app/app.component.ts
@@ -10,14 +10,28 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   
+  // primitive types
   title!: string;
+  name: string = "Babu Miya";
+  age: number = 30;
+  isMale: boolean = true;
+
+  // non primitive type
+
+  list : string[] = ['india','usa','uk','uae'];
+  listNumbers:number[] = [1,2,3,4,5];
+  object:object = {name: 'sameer', age: 30};
+
+  //other types
+
 
   constructor() {
 
   }
 
   ngOnInit(): void {
-    // this.title = "Basics Of Type Script";
+    //this.title = true; //Type 'boolean' is not assignable to type 'string'.
+    this.title = "Basics Of Type Script";
     console.log(this.title);
   }
 
