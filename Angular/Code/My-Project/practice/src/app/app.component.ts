ba946a3e8a8c5c8172df8e0414942c6136c5c999
moba0222 <mohammad.babu.miya@netcracker.com>
2024-04-15 17:55:25 +0530

    10-04-2024 - tech learning starting-002

--- Angular/Code/My-Project/practice/src/app/app.component.ts
+++ Angular/Code/My-Project/practice/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,11 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'practice';
+export class AppComponent implements OnInit {
+
+  title!:string;
+
+  ngOnInit() {
+    this.title = 'practice';
+  }
 }
