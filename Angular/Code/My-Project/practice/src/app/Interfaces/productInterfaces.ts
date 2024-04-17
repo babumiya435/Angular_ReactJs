ec3f9400f3cb6cd77c975abca58db924f4521beb
moba0222 <mohammad.babu.miya@netcracker.com>
2024-04-17 14:40:11 +0530

    17-04-2024 - tech learning starting-003

--- Angular/Code/My-Project/practice/src/app/Interfaces/productInterfaces.ts
+++ Angular/Code/My-Project/practice/src/app/Interfaces/productInterfaces.ts
@@ -1,13 +1,27 @@
+export type PriceTag = {
+    priceTagId?: string,
+    priceTagName: string,
+  }
+
 
 export interface Price {
     value: number;
     currency: string;
 }
 
-export interface Product {
+// --> we can include other types or interfaces as child custom types
+export interface ProductInt {
     id: string;
     title: string;
     description: string;
     price: Price;
     image?: string;
+}
+
+// extend types using interfaces
+// --> we can extend multiple interfaces
+// --> we can also extend types + intefaces
+export interface PurchasedProductInt extends ProductInt, Price, PriceTag {
+    orderId?: string,
+    orderDate?: Date,
 }
